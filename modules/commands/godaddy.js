33c3e596b8761aa2ccffeeba170405fdee966959
Guilherme Pereira <guilherme.pereira@ndrive.com>
2015-07-13 19:09:27 +0100

    Working add dns

--- modules/commands/godaddy.js
+++ modules/commands/godaddy.js
@@ -29,327 +29,80 @@ commands.godaddy = {
     { usage: '--KEY VALUE' }
   ],
   run: function (args) {
-    var user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1944.0 Safari/537.36';
+    var pages = [ { "url":"https://dns.godaddy.com/ZoneFile.aspx?zone=babel.school&zoneType=0&refer=dcc&prog_id=GoDaddy",
 
-    phantom.create('--ignore-ssl-errors=true', function (ph) {
+    var actions = ["handle_page", "add_elements", "select_type_record", "finish"];
+
+    onloadfinished(function() { page.includejs('', function() { $().click() }) })
+
+    phantom.create('--load-images=no', '--ignore-ssl-errors=true', function (ph) {
       ph.createPage(function (page) {
-        page.set('settings', {userAgent: user_agent}, function(){
-          page.open("https://dns.godaddy.com/ZoneFile.aspx?zone=NDRIVE.COM&zoneType=0&refer=dcc&prog_id=GoDaddy", function (open_err, status) {
+        page.set('settings.userAgent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36');
+
+        function handle_page(file) {
+          page.open(file, function() {
             page.includeJs("https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js", function() {
-              console.log("JS Included");
-              page.render('aaaa0-entry.jpg');
               page.evaluate(function(){
                 $('#Login_userEntryPanel2_UsernameTextBox').val('');
                 $('#Login_userEntryPanel2_PasswordTextBox').val('');
                 $("#LoginImageButton_div").click();
-
-                return true;
               }, function(err, result){
-                page.render('aaaa1-entry.jpg');
-                console.log($('body').html());
-                ph.exit();
+                setTimeout(next_page, 4000);
               });
             });
           });
-        });
+        }
+
+        function add_elements() {
+          page.evaluate(function(){
+            $('#divAddRecord').click();
+          }, function(err, result){
+            setTimeout(next_page, 1000);
+          });
+        }
+
+        function select_type_record() {
+          page.evaluate(function(){
+            $("#ctl00_cphMain_ctl00_ddlRecordType", $("#ifrm").contents()).val("A");
+            setTimeout($("#ctl00_cphMain_ctl00_ddlRecordType", $("#ifrm").contents()).trigger("change"), 100);
+            $("#ctl00_cphMain_ctl00_txtARecordHost", $("#ifrm").contents()).val("zzz");
+            $("#ctl00_cphMain_ctl00_txtARecordPointsto", $("#ifrm").contents()).val("192.168.1.26");
+            setTimeout($("#ctl00_cphMain_ctl00_btnOK", $("#ifrm").contents()).click(), 100);
+            setTimeout($("#ctl00_cphMain_divSaveOne .cssblack-button").click(), 100);
+          }, function(err, result){
+            setTimeout(next_page, 1000);
+          });
+        }
+
+        function finish() {
+          page.evaluate(function(){
+            $("#modalOkTwo").click();
+          }, function(err, result){
+            setTimeout(next_page, 1000);
+          });
+        }
+
+        function next_page() {
+          var file=pages.shift();
+          if(file === undefined){
+            page.close();
+            ph.exit(0);
+          } else {
+            console.log(file);
+            if (file.action === "handle_page"){
+              handle_page(file.url);
+            } else if (file.action === "add_elements") {
+              add_elements();
+            } else if (file.action === "select_type_record") {
+              select_type_record();
+            } else if (file.action === "finish") {
+              finish();
+            }
+          }
+        }
+        next_page();
       });
     });
-
-
-
-
-
-    // phantom.create('--ignore-ssl-errors=true', function (ph) {
-
-    //   function renderPage(url, ph) {
-    //     var redirectURL = null;
-
-    //     ph.createPage(function (page) {
-    //       page.onConsoleMessage(function(msg) {
-    //         console.log("onConsole: " + msg);
-    //       });
-
-    //       page.onResourceReceived(function(resource) {
-    //         if (url == resource.url && resource.redirectURL) {
-    //           redirectURL = resource.redirectURL;
-    //         }
-    //       });
-
-    //       page.open(url, function(status) {
-    //         if (redirectURL) {
-    //           console.log("redirected");
-    //           renderPage(redirectURL);
-    //         } else if (status == 'success') {
-    //           console.log("success");
-    //         }
-    //       });
-    //     });
-    //   }
-
-    //   renderPage("http://www.google.com", ph)
-    // });
-
-
-
-
-
-
-
-
-  // Attempt 2nd
-
-    //   phantom.create('--ignore-ssl-errors=true', function (ph) {
-    //   ph.createPage(function (page) {
-
-
-
-    //     setTimeout(function() {
-    //       page.open("https://sso.godaddy.com", function () {
-    //           page.render('aaaa-entry.png');
-
-    //           page.evaluate(function () {
-    //             document.querySelector('input[id=username]').value = '';
-    //             document.querySelector('input[id=password]').value = '';
-
-    //             var ev = document.createEvent("MouseEvents");
-    //             ev.initEvent("click", true, true);
-    //             document.querySelector("button[id=submitBtn]").dispatchEvent(ev);
-    //           });
-
-    //           console.log("........");
-    //           page.render('aaaa2-entry.png');
-    //       });
-    //     }, 0);
-
-    //     setTimeout(function() {
-    //       console.log("### STEP 2: ");
-    //       page.open("https://dns.godaddy.com/ZoneFile.aspx?zone=NDRIVE.COM&zoneType=0&refer=dcc&prog_id=GoDaddy", function () {
-    //         page.render('aaaa4-entry.png');
-    //       });
-
-    //       setTimeout(function(){
-    //           ph.exit();
-    //       }, 100);
-    //     }, 5000);
-    //   });
-    // });
-
-    //     var failExercise = function(msg) {
-    //         // exercise.emit('fail', msg);
-    //         callback(null, false);
-    //         ph.exit();
-    //     };
-
-    //     page.onConsoleMessage(function(msg) {
-    //         console.log(msg);
-    //         page.render('aaaa4-entry.png');
-    //         if (msg === 'prevented') {
-    //             // exercise.emit('pass', 'DOM-configures');
-    //             // callback(null, true);
-    //             ph.exit();
-    //         } else if (msg === 'finished') {
-    //             failExercise('failed exercise');
-    //         }
-    //     });
-
-
-
-
-
-    //     page.open("https://sso.godaddy.com", function () {
-    //         page.render('aaaa-entry.png');
-
-
-    //       page.includeJs("https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js", function() {
-
-
-    //         page.evaluate(function () {
-    //           document.querySelector('input[id=username]').value = '';
-    //           document.querySelector('input[id=password]').value = '';
-    //         });
-
-    //         // console.log("........");
-    //         // page.render('aaaa2-entry.png');
-
-    //         // // page.includeJs("https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js", function() {
-    //         //   page.evaluate(function () {
-    //         //     console.log('click?');
-    //         //     console.log(document.querySelector('input[id=username]').text);
-    //         //     $('#forgotUsernameLink').click();
-    //         //     // $('#login-form :submit').click();
-    //         //     setTimeout(function(){
-    //         //         // page.render('aaaa4-entry.png');
-    //         //         console.log('prevented');
-    //         //     }, 2000);
-    //         //     // var ev = document.createEvent("MouseEvents");
-    //         //     // ev.initEvent("click", true, true);
-
-    //         //     // console.log(document.querySelector("button[id=submitBtn]").dispatchEvent(ev));
-
-    //         //     // document.querySelector("button[id=submitBtn]").dispatchEvent(ev);
-    //         //   })
-    //         // // })
-
-    //         console.log("........");
-    //         page.render('aaaa3-entry.png');
-    //     });
-
-    //     // setTimeout(function() {
-    //     //   console.log("### STEP 2: ");
-    //     //   page.open("https://dns.godaddy.com/ZoneFile.aspx?zone=NDRIVE.COM&zoneType=0&refer=dcc&prog_id=GoDaddy", function () {
-    //     //     page.render('aaaa4-entry.png');
-    //     //   });
-
-    //     //   setTimeout(function(){
-    //     //       ph.exit();
-    //     //   }, 100);
-    //     // }, 5000);
-    //   });
-    // });
-
-
-
-    // var options = {
-    //   channel: args.channel || '#alerts',
-    //   icon_emoji: args['icon-emoji'] || ':cloud:',
-    //   icon_url: args['icon-url'] || null,
-    //   text: args.message,
-    //   username: args.user || 'Overcast'
-    // };
-
-    // var custom_fields = _.extend({}, args);
-    // var keys = ['_', 'channel', 'command', 'message', 'icon-emoji', 'icon-url', 'message', 'user'];
-    // _.each(keys, function (key) {
-    //   delete custom_fields[key];
-    // });
-
-    // options.fields = custom_fields;
-
-    // exports.send(options);
-
-
-
-
-
-    // attempt 3rd
-
-          // ph.createPage(function (page) {
-      //   page.onConsoleMessage(function(msg) {
-      //     // if (msg === 'finished') {
-      //     //   console.log("onConsole: " + msg);
-      //     //   page.render('aaaa2-entry.jpg', {format: 'jpeg', quality: '100'});
-      //     //   setTimeout(function () {
-      //     //     handle_page_two();
-      //     //   }, 2000);
-      //     // } else {
-      //       console.log("onConsole: " + msg);
-      //     // }
-      //   });
-
-      //   // function handle_page(){
-
-      //   //   page.open("https://sso.godaddy.com", function(status) {
-      //   //     if ( status === "success" ) {
-      //   //       page.includeJs("https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js", function() {
-      //   //         page.evaluate(function() {
-      //   //           $('#username').val('');
-      //   //           $('#password').val('');
-      //   //           $("#submitBtn").click();
-      //   //           console.log("finished");
-      //   //         });
-      //   //       });
-      //   //     }
-      //   //   });
-      //   // }
-
-      //   function handle_page_two(){
-      //     console.log("page two handle");
-
-      //     page.open("https://dns.godaddy.com/ZoneFile.aspx?zone=NDRIVE.COM&zoneType=0&refer=dcc&prog_id=GoDaddy", function(status){
-      //       page.includeJs("https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js", function() {
-
-      //         page.render('aaaa0-entry.jpg');
-
-      //         page.evaluate(function() {
-      //           $('#Login_userEntryPanel2_UsernameTextBox').val('');
-      //           $('#Login_userEntryPanel2_PasswordTextBox').val('');
-      //           $("#LoginImageButton_div").click();
-
-      //           setTimeout(function () {
-      //             console.log("will click add record");
-      //             console.log("=======================================================");
-      //             console.log($("body").html());
-      //             console.log("=======================================================");
-      //           }, 1000);
-      //           // $('#foo').length === 0
-
-      //           // $('#divAddRecord').click();
-
-      //           // // console.log("check elements");
-      //           // $('#ctl00_cphMain_ctl00_ddlRecordType').val("A");
-
-      //           // // console.log("check elements part 2");
-
-      //           // // console.log($('#ctl00_cphMain_ctl00_txtARecordHost').html());
-
-      //           // $('#ctl00_cphMain_ctl00_txtARecordHost').val("zzz");
-      //           // $('#ctl00_cphMain_ctl00_txtARecordPointsto').val("176.58.123.102");
-      //           // $('#ctl00_cphMain_ctl00_btnOK').click();
-
-      //           // // console.log($('#ctl00_cphMain_ctl00_btnOK').html());
-
-      //           // // console.log("adds ip address");
-
-      //           // $('.cssblack-button').click();
-
-      //           // // console.log($('.cssblack-button').html());
-      //           // // console.log("confirm click");
-
-      //           // // console.log($('#modalOkTwo').html());
-      //           // $('#modalOkTwo').click();
-
-      //         });
-      //       });
-      //     });
-      //   }
-
-
-      //   function exit_page(){
-      //     // console.log("State : 4" + ph.state());
-      //     // setTimeout(function () {
-      //       // page.evaluate(function() {
-      //         console.log("finalized");
-      //         page.render('aaaa10-entry.jpg', {format: 'jpeg', quality: '100'});
-      //         ph.exit();
-      //       // });
-      //     // }, 3000);
-      //   }
-
-      //   function next_page(){
-      //       console.log("start page 2");
-      //       handle_page_two();
-
-      //       // setTimeout(function () {
-      //       //   console.log("finish page 2");
-      //       //   handle_page_two();
-      //       // }, 10000);
-
-      //       setTimeout(function(){
-      //         exit_page();
-      //       }, 10000)
-      //   }
-
-      //   next_page();
-      // });
-
-
-
-
-
-
-
-
-
   }
 };
 
